4a46d6fae66164d913d41e48a797ede5ca39047d
许晓周 <759811542@qq.com>
2025-03-19 15:07:09 +0800

    v1

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # define_api
-定义api
+
+定义 api
+
+1. 通过 typescript 定义接口
+2. 生成 openapi schema
+3. 生成文档
+4. 生成前端接口定义
+5. 自动测试接口返回数据
---
+++ type.ts
@@ -0,0 +1,70 @@
+type BaseApi<
+  T extends {
+    url: string;
+    method: "GET" | "POST" | "PUT" | "DELETE";
+    paths?: Record<string, string> | undefined;
+    params: {};
+    body: {};
+    response: {};
+  }
+> = {
+  url: T["url"];
+  method: T["method"];
+  paths: T["paths"];
+  params: T["params"];
+  body: T["body"];
+  response: T["response"];
+};
+
+/**
+ * 性别枚举
+ */
+enum Sex {
+  /**
+   * 男
+   */
+  Man = "man",
+  /**
+   * 女
+   */
+  Woman = "woman",
+}
+
+type User = {
+  /**
+   * id
+   * @default "2"
+   */
+  id: string;
+  // 姓名
+  name: string;
+  /**
+   * 性别
+   */
+  sex: Sex;
+};
+
+// Get user by id
+type GetUserById = BaseApi<{
+  url: "/users/{userId}";
+  method: "GET";
+  paths: {
+    /**
+     * 用户id
+     */
+    userId: string;
+  };
+  params: {
+    /**
+     * 用户名
+     */
+    name?: string;
+  };
+  body: {
+    /**
+     * 文件
+     */
+    file: FormData | string;
+  };
+  response: { 200: User };
+}>;
